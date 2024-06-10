7398624abfba16b2c91cfa1c2851423ec2a2a7d5
Raymond1030 <loho1030@gmail.com>
2024-06-10 21:10:23 +0800

    Add README

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,11 +38,13 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(files("./libs/BaiduLBS_Android.aar"))
+
 //    implementation(fileTree(mapOf(
 //        "dir" to "C:\\ProgramData\\Astudy\\PKU\\Distributed-computing-of-spatial-data\\CloudMapMobileApp\\app\\libs",
 //        "include" to listOf("*.aar", "*.jar")
 //    )))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1");
+    implementation 'com.squareup.okhttp3:okhttp:4.9.0'
 }
---
+++ app/src/main/java/com/example/cloudmapmobileapp/CoordinateConverter.java
@@ -0,0 +1,2 @@
+package com.example.cloudmapmobileapp;public class CoordinateConverter {
+}
